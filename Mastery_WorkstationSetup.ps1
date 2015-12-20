49a3c5eb066adecf2719cbc362b6ad9a803545e3
Frison B Alexander <fri.b.alex@gmail.com>
2015-12-20 13:32:17 -0500

    Initial Commit. GL!
    May the force be with you

---
+++ Mastery_WorkstationSetup.ps1
@@ -0,0 +1,18 @@
+﻿#install Chocolatey
+#iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#git
+#choco -y install git
+#git config --global user.name "Frison Alexander"
+#git config --global user.email "fri.b.alex@gmail.com"
+#TODO: grab ssh key from github account
+#      github for windows looks like a better way
+
+#install vim and plugins
+
+#install autohotkey and scripts
+
+
+#Change Capital to Ctrl
+
+
